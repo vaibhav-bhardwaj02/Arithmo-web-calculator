86592cf680bef2bce94b1688b3da3da8938ebe23
Vaibhav Sharma <bhardwajvaibhav9760@gmail.com>
2026-01-17 23:22:30 +0530

    added display formatting

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const toggleBtn = document.getElementById("toggleLayout");
 let expression = "";
 let current = "";
 let justCalculated = false;
+
+const MAX_DISPLAY_LENGTH = 12;
+
+// ---------- Calculator Logic ----------
+function formatResult(num) {
+  let str = num.toString();
+
+  if (str.length > MAX_DISPLAY_LENGTH) {
+    if (str.includes(".")) {
+      const [int, dec] = str.split(".");
+      const allowedDecimals = MAX_DISPLAY_LENGTH - int.length - 1;
+
+      if (allowedDecimals > 0) {
+        str = Number(num).toFixed(allowedDecimals);
+      } else {
+        str = Number(num).toExponential(5);
+      }
+    } else {
+      str = Number(num).toExponential(5);
+    }
+  }
+  return str;
+}
